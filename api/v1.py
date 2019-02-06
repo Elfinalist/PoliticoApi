5f35d56d800ca0d1bef2e557b655a57118b3beae
Elvis Wanjohi <elfinalist@gmail.com>
2019-02-07 01:07:35 +0300

    - implement create political party - implement get political party - implement delete political party

--- api/v1.py
+++ api/v1.py
@@ -1,7 +1,69 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+import json
+from api.models import Politico, InputError
+
+politico = Politico()
 
 v1 = Blueprint('v1_api', __name__, url_prefix='/api/v1')
 
 @v1.route("/")
 def hello():
     return "Hello World!"
+
+
+@v1.route("/parties", methods=["POST", "GET"])
+def parties():
+    response = {}
+    if(request.method == 'POST'):
+        try:
+            party_data = request.json
+            party_name = party_data["name"]
+            party_hq = party_data["hqAddress"]
+            party_logo = party_data["logoUrl"]
+
+            # create political party
+            party = politico.create_political_party(party_name, party_hq, party_logo)
+            response["status"] = 201
+            response["data"] = party
+            return jsonify(response)
+        except InputError as error:
+            response["status"] = 400
+            response["error"] = error.message
+            return jsonify(response), response["status"]
+
+    # get all political parties
+    elif(request.method == 'GET'):
+        political_parties = politico.get_political_parties()
+        response["status"] = 200
+        response["data"] = political_parties
+        return jsonify(response), response["status"]
+    else:
+        pass
+
+@v1.route("/parties/<int:id>", methods=["GET", "DELETE", "PUT"])
+def party(id):
+    response = {}
+    #get one party
+    if(request.method == 'GET'):
+        p_party = politico.get_political_party(id)
+        if(len(p_party) == 0):
+            response["status"] = 404
+            response["error"] = "political party not found"
+        else:
+            response["status"] = 200
+            response["data"] = p_party
+        return jsonify(response), response["status"]
+    #delete party
+    elif(request.method =='DELETE'):
+        deleted = politico.delete_political_party(id)
+        if(deleted):
+            response["status"] = 200
+            response["data"] = {
+                "message": "political party sucessfully deleted"
+            }
+        else:
+            response["status"] = 404
+            response["error"] = "political party not found"
+        return jsonify(response), response["status"]
+
+
--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,31 @@
+import json
 def test_index(client):
     response = client.get('/api/v1/')
     assert response.status_code == 200
+
+def test_create_political_party(client):
+    payload = {
+        "name": "Kanu",
+        "hqAddress": "Nakuru",
+        "logoUrl": ""
+    }
+    response = client.post('/api/v1/parties',data = json.dumps(payload), content_type='application/json')
+    response_data = response.get_json()
+    assert response_data["status"] == 201
+    assert response_data["data"]["name"] == "Kanu"
+    assert response_data["data"]["hq"] == "Nakuru"
+    assert response_data["data"]["logoUrl"] == ""
+    assert "id" in response_data["data"]
+    
+
+def test_get_political_party(client):
+    response = client.get('/api/v1/parties/1', content_type='application/json')
+    response_data = response.get_json()
+    assert "id" in response_data["data"]
+    assert response_data["data"]["id"] == 1
+
+
+def test_delete_political_party(client):
+    response = client.delete('/api/v1/parties/1', content_type='application/json')
+    response_data = response.get_json()
+    assert response_data["data"]["message"] == "political party sucessfully deleted"
